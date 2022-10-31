ece73cc5a755a53379ffd2dfc98b92634bf2ef01
s.pafianov <s.pafianov@SERVICE>
2022-10-31 21:52:00 +0200

    Done #1,2 task

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 #include<iostream>
 #include<math.h>
 #include<Windows.h>
 using namespace std;
 
+void exponention(int x, int y, int res) //Піднесення до степеня 
+{
+	
+	for (size_t i = 1; i < y; i++)
+	{
+		res = x * res;
+	}
+	cout << "Res = " << res << endl;
+}
+void range_sum(int x,int y,int res) //повертає суму чисел із діапазону між ними
+{
+	for (size_t i = x; i <= y; i++)
+	{
+		res += i;
+	}
+	cout << "Sum = " << res << endl;
+}
+
 int main() 
 {
-
+	
 	return 0;
 }
